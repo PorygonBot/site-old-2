6bdcfebe5621ab5467f4ab0480cd7047a2bc03f9
harbar20 <harbar2021@gmail.com>
2020-12-18 23:41:04 -0500

    finished header! only in dark mode though

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,84 @@
+import { useState } from "react";
+import useSWR from "swr";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
+import oauth from "./oauth";
+import { deleteTokens, getTokensForBrowser } from "../lib/cookie";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Layout({ children }) {
+    let { data, error } = useSWR("/api/user", fetcher);
+    const router = useRouter();
+    const user = data;
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const url = oauth.generateAuthUrl({
+        scope: ["identify", "guilds"],
+    });
+
+    const logout = async (e) => {
+        const tokens = getTokensForBrowser();
+        const credentials = Buffer.from(
+            `${process.env.DISCORD_CLIENT_ID}:${process.env.DISCORD_CLIENT_SECRET}`
+        ).toString("base64");
+        await oauth.revokeToken(tokens.access_token, credentials);
+        deleteTokens();
+        setIsLoading(true);
+        router.reload();
+    };
+
+    if (isLoading === true || !user) {
+        return <h1>Loading...</h1>;
+    }
+
+    const guildNames =
+        user && user.guilds ? user.guilds.map((guild) => guild.name) : [];
+
     return (
-    <div className="bg-red-500">
-        { children }
-    </div>)
+        <div>
+            <div className="bg-black w-full flex justify-between p-2 items-center">
+                <div className="flex text-white items-center">
+                    <Link href="/">
+                        <a>
+                            <div className="inline-flex">
+                                <Image
+                                    src="/PorygonLogoCircle.png"
+                                    width={50}
+                                    height={50}
+                                />
+                            </div>
+                        </a>
+                    </Link>
+                    <ul className="inline-flex text-white text-2xl ml-3">
+                        <Link href="https://discord.com/api/oauth2/authorize?client_id=692091256477581423&permissions=0&scope=bot"><a><li className="ml-3 mr-2">Invite</li></a></Link>
+                        <Link href="https://discord.gg/ZPTMZ8f"><a><li className="ml-3 mr-2">Server</li></a></Link>
+                        <Link href="https://bit.ly/porygon"><a><li className="ml-3 mr-2">Documentation</li></a></Link>
+                    </ul>
+                </div>
+
+                <div classname="items-center">
+                    {!user.available ? (
+                        <button
+                            onClick={(e) => router.push(url)}
+                            className="p-3 w-24 h-12 text-pink-400 hover:text-white text-sm text-center font-bold rounded-lg border-2 border-pink-400 bg-transparent hover:bg-pink-400"
+                        >
+                            Login
+                        </button>
+                    ) : (
+                        <button
+                            onClick={logout}
+                            className="p-3 w-24 h-12 text-pink-400 hover:text-white text-sm text-center font-bold rounded-lg border-2 border-pink-400 bg-transparent hover:bg-pink-400"
+                        >
+                            Logout
+                        </button>
+                    )}
+                </div>
+            </div>
+            {children}
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,27 @@
 import { useState } from "react";
 import useSWR from "swr";
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
 import Layout from "../components/layout";
-import oauth from '../components/oauth'
-import {deleteTokens, getTokensForBrowser} from '../lib/cookie'
+import oauth from "../components/oauth";
+import { deleteTokens, getTokensForBrowser } from "../lib/cookie";
 
-const fetcher = url => fetch(url).then(r => r.json())
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Home() {
-	let {data, error} = useSWR('/api/user', fetcher);
-	const router = useRouter()
-	const user = data;
+    let { data, error } = useSWR("/api/user", fetcher);
+    const router = useRouter();
+    const user = data;
 
-	const [isLoading, setIsLoading] = useState(false);
-	
-	const url = oauth.generateAuthUrl({
-        scope: ["identify", "guilds"]
-	});
-	const buttonCSS = "h-40 w-40 bg-blue-200"
+    const [isLoading, setIsLoading] = useState(false);
 
-	const logout = async (e) => {
-		const tokens = getTokensForBrowser();
-		const credentials = Buffer.from(`${process.env.DISCORD_CLIENT_ID}:${process.env.DISCORD_CLIENT_SECRET}`).toString("base64");
-		await oauth.revokeToken(tokens.access_token, credentials);
-		deleteTokens();
-	}
-
-	/*
-	You want to store the loading status in the state.
-	Set it to false. 
-	When you click logout, set that to true. 
-	Once the function is resolved, set to false again.
-
-	oh that's smart. can you show me lol, i came up with a really jank solution for loading stuff.
-	
-	yeah you can use the useState()
-	*/
-	
-	if (!user) {
-		return <h1>Loading...</h1>
-	}
-
-	const guildNames = user && user.guilds ? user.guilds.map((guild) => guild.name) : []
+    if (isLoading === true || !user) {
+        return <h1>Loading...</h1>;
+    }
 
     return (
         <Layout>
-            <h1 className="text-5xl font-bold text-purple-500">
-                Hello, {user.available ? user.username : "world"}!
-            </h1>
-			<ul>
-			{ 
-				guildNames.map((guildName) => <li>{guildName}</li>)
-			}
-			</ul>
-            {
-				!user.available ? <a href={url}><button className={buttonCSS}>Login</button></a> : <button onClick={logout} className={buttonCSS}>Logout</button>
-			}
+            <p>Hello, {user.available ? user.username : "world"}!</p>
         </Layout>
     );
 }
